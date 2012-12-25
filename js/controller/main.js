0a96a8b7de88e4e8b604d5fa803b8e91a641623c
Samuel Messing <samuel.messing@gmail.com>
2012-12-25 18:17:13 -0500

    starting js dir, working on closure's xhr demo.

---
+++ js/controller/main.js
@@ -0,0 +1,26 @@
+goog.provide('controller');
+goog.require('goog.net.XhrIo');
+
+
+/**
+ * Entry point into the program.
+ */
+function main() {
+  alert('it works!');
+}
+
+/**
+ * Retrieve JSON data using XhrIo's static send() method.
+ *
+ * @param {string} dataUrl The url to request.
+ */
+function getData(dataUrl) {
+  log('Sending simple request for ['+ dataUrl + ']');
+  goog.net.XhrIo.send(dataUrl, function(e) {
+    var xhr = e.target;
+    var obj = xhr.getResponseJson();
+    log('Received Json data object with title property of "' +
+        obj['title'] + '"');
+    alert(obj['content']);
+  });
+}
