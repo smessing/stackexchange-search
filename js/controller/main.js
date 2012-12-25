41009225a88579a16ca590f315455ea36eb2fd8e
Samuel Messing <samuel.messing@gmail.com>
2012-12-25 18:42:28 -0500

    basic xhr functionality in place, need to actually work on displaying data, etc.

--- js/controller/main.js
+++ js/controller/main.js
@@ -6,7 +6,21 @@ goog.require('goog.net.XhrIo');
  * Entry point into the program.
  */
 function main() {
-  alert('it works!');
+}
+
+controller.BASE_URL = 'https://api.stackexchange.com/2.1/';
+controller.SEARCH_URL =
+  controller.BASE_URL + 'search?order=desc&sort=activity&intitle=';
+controller.URL_END = '&site=stackoverflow';
+
+controller.search = function() {
+  var searchBar = document.getElementById('search-bar');
+  url = controller.buildUrl(searchBar.value);
+  data = controller.getData(url);
+}
+
+controller.buildUrl = function(searchText) {
+  return controller.SEARCH_URL + searchText + controller.URL_END;
 }
 
 /**
@@ -14,13 +28,17 @@ function main() {
  *
  * @param {string} dataUrl The url to request.
  */
-function getData(dataUrl) {
+controller.getData = function(dataUrl) {
   log('Sending simple request for [' + dataUrl + ']');
   goog.net.XhrIo.send(dataUrl, function(e) {
     var xhr = e.target;
     var obj = xhr.getResponseJson();
-    log('Received Json data object with title property of "' +
+    controller.log('Received Json data object with title property of "' +
         obj['title'] + '"');
-    alert(obj['content']);
   });
+}
+
+controller.log = function(msg) {
+  document.getElementById('log').appendChild(document.createTextNode(msg));
+  document.getElementById('log').appendChild(document.createElement('br'));
 }
