fab084752d9f5f3dd1bd1625e2751354f7141a0d
Samuel Messing <samuel.messing@gmail.com>
2012-12-25 18:46:18 -0500

    adding more doc strings.

--- js/controller/main.js
+++ js/controller/main.js
@@ -13,24 +13,31 @@ function main() {
  */
 controller.BASE_URL = 'https://api.stackexchange.com/2.1/';
 
-/*
+/**
  * Url to use for searching StackOverflow answers, just append search text.
  */
 controller.SEARCH_URL =
   controller.BASE_URL + 'search?order=desc&sort=activity&intitle=';
 
-/*
+/**
  * Specifies using the StackOverflow site for API requests.
  */
 controller.SITE_URL = '&site=stackoverflow';
 
+/**
+ * Search StackExchange site based on text in the search bar.
+ */
 controller.search = function() {
   var searchBar = document.getElementById('search-bar');
-  url = controller.buildUrl(searchBar.value);
-  data = controller.getData(url);
+  searchUrl = controller.buildSearchUrl(searchBar.value);
+  data = controller.getData(searchUrl);
 }
 
-controller.buildUrl = function(searchText) {
+/**
+ * Build a search url.
+ * @param {string} searchText The text to use in the search.
+ */
+controller.buildSearchUrl = function(searchText) {
   return controller.SEARCH_URL + searchText + controller.URL_END;
 }
 
