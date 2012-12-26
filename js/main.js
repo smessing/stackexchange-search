6b0a88191b3a4eccca6777d593f62fa3a064d31d
Samuel Messing <samuel.messing@gmail.com>
2012-12-26 15:47:35 -0500

    fixing jsstyle.

--- js/main.js
+++ js/main.js
@@ -2,7 +2,11 @@ goog.provide('main');
 
 goog.require('control.Controller');
 
+/**
+ * The main function---the entry point into the application.
+ * @constructor
+ */
 main.main = function() {
   console.log(control.Controller);
   this.controller = new control.Controller();
-}
+};
